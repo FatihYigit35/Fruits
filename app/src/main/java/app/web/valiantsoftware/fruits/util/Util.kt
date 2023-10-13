670a49bc8ea6d0a7af8f86ad84a5cd381a0fa30c
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-10-13 15:37:16 +0300

    Images fetched with glide

---
+++ app/src/main/java/app/web/valiantsoftware/fruits/util/Util.kt
@@ -0,0 +1,21 @@
+package app.web.valiantsoftware.fruits.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import app.web.valiantsoftware.fruits.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+fun ImageView.imageDownload(url: String?, placeholder: CircularProgressDrawable){
+    val options = RequestOptions().placeholder(placeholder).error(R.mipmap.ic_launcher_round)
+    Glide.with(context).setDefaultRequestOptions(options).load(url).into(this)
+}
+
+fun createPlaceholder(context: Context): CircularProgressDrawable{
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
