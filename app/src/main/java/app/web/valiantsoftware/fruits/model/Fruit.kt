e62b7d12e3255ea1cfb87ee42c4b3b74092edb7f
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-10-13 15:34:08 +0300

    SerializedName implemented

--- app/src/main/java/app/web/valiantsoftware/fruits/model/Fruit.kt
+++ app/src/main/java/app/web/valiantsoftware/fruits/model/Fruit.kt
@@ -1,11 +1,22 @@
 package app.web.valiantsoftware.fruits.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Fruit(
+    //Using @SerializedName, we match the variables in the json file with the variables in our class.
+    //It may not be necessary to use variable names if they are the same.
+    @SerializedName("name")
     val name: String?,
+    @SerializedName("description")
     val description: String?,
+    @SerializedName("calories")
     val calories: String?,
+    @SerializedName("carbohydrate")
     val carbohydrate: String?,
+    @SerializedName("protein")
     val protein: String?,
+    @SerializedName("fat")
     val fat: String?,
+    @SerializedName("image")
     val image: String?
 ) {}
