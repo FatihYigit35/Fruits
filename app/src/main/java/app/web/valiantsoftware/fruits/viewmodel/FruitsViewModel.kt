bd6c3a6274a219e3abde7c953f997afe1143f3c8
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-10-17 16:37:08 +0300

    add room save codes

--- app/src/main/java/app/web/valiantsoftware/fruits/viewmodel/FruitsViewModel.kt
+++ app/src/main/java/app/web/valiantsoftware/fruits/viewmodel/FruitsViewModel.kt
@@ -1,20 +1,24 @@
 package app.web.valiantsoftware.fruits.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import app.web.valiantsoftware.fruits.model.Fruit
+import app.web.valiantsoftware.fruits.service.AppDatabase
 import app.web.valiantsoftware.fruits.service.FruitAPIService
+import app.web.valiantsoftware.fruits.util.AppSharedPreferences
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class FruitsViewModel : ViewModel() {
+class FruitsViewModel(application: Application) : BaseViewModel(application) {
     val fruitsLiveData = MutableLiveData<List<Fruit>>()
     val errorMessageLiveData = MutableLiveData<Boolean>()
     val progressBarLiveData = MutableLiveData<Boolean>()
     private val fruitApiService = FruitAPIService()
     private val disposable = CompositeDisposable()
+    private val sharedPreferences = AppSharedPreferences(getApplication())
 
     fun refreshData() {
         getDataFromRetrofit()
@@ -30,9 +34,7 @@ class FruitsViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<Fruit>>() {
                     override fun onSuccess(t: List<Fruit>) {
-                        fruitsLiveData.value = t
-                        errorMessageLiveData.value = false
-                        progressBarLiveData.value = false
+                        insertFruitsToRoom(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -43,5 +45,22 @@ class FruitsViewModel : ViewModel() {
 
                 })
         )
+    }
+
+    private fun showFruits(fruits: List<Fruit>){
+        fruitsLiveData.value = fruits
+        errorMessageLiveData.value = false
+        progressBarLiveData.value = false
+    }
+
+    private fun insertFruitsToRoom(fruits: List<Fruit>){
+        launch {
+            val dao = AppDatabase(getApplication()).fruitDao()
+            dao.deleteAllFruit()
+            dao.insertAll(*fruits.toTypedArray())
+            showFruits(dao.getAllFruit())
+        }
+
+        sharedPreferences.saveTime(System.nanoTime())
     }
 }
