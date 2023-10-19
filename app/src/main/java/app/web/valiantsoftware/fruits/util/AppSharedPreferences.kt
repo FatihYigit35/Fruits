b3ce6e5500fd8efcec06fc8ca7ef6383a28c30c3
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-10-19 17:12:40 +0300

    code revised

--- app/src/main/java/app/web/valiantsoftware/fruits/util/AppSharedPreferences.kt
+++ app/src/main/java/app/web/valiantsoftware/fruits/util/AppSharedPreferences.kt
@@ -2,37 +2,36 @@ package app.web.valiantsoftware.fruits.util
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.content.res.Resources
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
-import app.web.valiantsoftware.fruits.R
 
 class AppSharedPreferences {
     companion object {
         private var sharedPreferences: SharedPreferences? = null
-        private var resources: Resources? = null
-
+        private var TIMEKEY = "last_refresh_time"
 
         @Volatile
         private var instance: AppSharedPreferences? = null
 
         private val lock = Any()
-        operator fun invoke(context: Context): AppSharedPreferences = instance ?: synchronized(lock) {
-            instance ?: createSharedPreferences(context).also {
-                instance = it
+        operator fun invoke(context: Context): AppSharedPreferences =
+            instance ?: synchronized(lock) {
+                instance ?: createSharedPreferences(context).also {
+                    instance = it
+                }
             }
-        }
 
         private fun createSharedPreferences(context: Context): AppSharedPreferences {
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-            this.resources = context.resources
             return AppSharedPreferences()
         }
     }
 
-    fun saveTime( time: Long){
-        sharedPreferences?.edit(commit = true){
-            putLong(resources?.getString(R.string.shared_preferences_key_time) ,time)
+    fun saveTime(time: Long) {
+        sharedPreferences?.edit(commit = true) {
+            putLong(TIMEKEY, time)
         }
     }
+
+    fun getTime() = sharedPreferences?.getLong(TIMEKEY,0)
 }
