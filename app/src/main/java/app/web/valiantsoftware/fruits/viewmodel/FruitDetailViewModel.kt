486eb97210f819d92a70dbb7411a7960d4646ed4
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-10-19 17:15:01 +0300

    The information of the clicked fruit was enabled to be displayed

--- app/src/main/java/app/web/valiantsoftware/fruits/viewmodel/FruitDetailViewModel.kt
+++ app/src/main/java/app/web/valiantsoftware/fruits/viewmodel/FruitDetailViewModel.kt
@@ -1,23 +1,19 @@
 package app.web.valiantsoftware.fruits.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import app.web.valiantsoftware.fruits.model.Fruit
+import app.web.valiantsoftware.fruits.service.AppDatabase
+import kotlinx.coroutines.launch
 
-class FruitDetailViewModel : ViewModel() {
+class FruitDetailViewModel(application: Application) : BaseViewModel(application) {
     val fruitLiveData = MutableLiveData<Fruit>()
-    fun getFruit() {
-        val banana = Fruit(
-            "Banana",
-            "A banana is the common name for a type of fruit and also the name for the herbaceous plants that grow it. " +
-                    "These plants belong to the genus Musa. " +
-                    "They are native to the tropical region of southeast Asia.",
-            "77",
-            "23g",
-            "1.1g",
-            "0.3g",
-            "https://raw.githubusercontent.com/FatihYigit35/JSONDatas/main/FruitsApp/banana.png"
-        )
-        fruitLiveData.value = banana
+    fun getFruit(uuid: Int) {
+        launch {
+            val dao = AppDatabase(getApplication()).fruitDao()
+            val fruit = dao.getFruit(uuid)
+            fruitLiveData.value = fruit
+        }
+
     }
 }
